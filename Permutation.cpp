70868391027450b25834f6161656493d5332d033
KhomenkoMax <khomenko1994@mail.ru>
2013-12-04 09:38:22 +0200

    Update Permutation.cpp

--- Permutation.cpp
+++ Permutation.cpp
@@ -40,6 +40,49 @@ class Permutation
          PermutationString[len+1]=0;        
 }
 
+
+long Permutation::getPermutationCode()
+{
+     return PermutationCode();
+     }
+     void Permutation::setPermutationCode(long newPermutationCode)
+     {
+     PermutationCode=newPermutationCode;
+     }
+char*Permutation::getPermutationString();
+{
+char*copyPermutationString;
+int len=strlen(PermutationString);
+copyPermutationString=new char[len+1];
+strncpy(copyPermutationString,PermutationString,len);
+copyPermutationString[len]=0;
+return copyPermutationString;
+}
+void Permutation::setPermutationStrig(char* newPermutationStrig)
+{
+if(PermutationString) delete[]PermutationString;
+int len=strlen(newPermutationString);
+PermutationString=new char[len+1];
+strncpy(PermutationString,newPermutationString,len);
+PermutationString[len]=0;    
+}
+char * getIndices();
+{
+char*copyIndices;
+int len=strlen(Indices);
+copyIndices=new char[len+1];
+strncpy(copyIndices,Indices,len);
+copyIndices[len]=0;
+return copyIndices;
+}
+void setIndices (char* newIndices);
+{
+if(Indices) delete[]Indices;
+int len=strlen(newIndices);
+Indices=new char[len+1];
+strncpy(Indices,newIndices,len);
+Indices[len]=0;    
+}
  system("PAUSE");
   return EXIT_SUCCESS;
 }
